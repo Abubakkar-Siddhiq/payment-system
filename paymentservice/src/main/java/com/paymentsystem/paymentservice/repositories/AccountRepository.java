f20caa8aaa3d924843d987fc8d16b08c7746b81f
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-03 19:55:15 +0530

    feat: Added Account and Transaction Repository

--- paymentservice/src/main/java/com/paymentsystem/paymentservice/repositories/AccountRepository.java
+++ paymentservice/src/main/java/com/paymentsystem/paymentservice/repositories/AccountRepository.java
@@ -1,4 +1,11 @@
 package com.paymentsystem.paymentservice.repositories;
 
-public class AccountRepository {
+import com.paymentsystem.paymentservice.domain.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
 }
