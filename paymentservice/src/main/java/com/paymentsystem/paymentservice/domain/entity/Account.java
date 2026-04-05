6bd2025112ab283cfeda9fae27a88220bc04a76a
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-05 19:34:33 +0530

    update(paymentservice): Added createdAt and PrePresist

--- paymentservice/src/main/java/com/paymentsystem/paymentservice/domain/entity/Account.java
+++ paymentservice/src/main/java/com/paymentsystem/paymentservice/domain/entity/Account.java
@@ -1,11 +1,12 @@
 package com.paymentsystem.paymentservice.domain.entity;
 
+import com.paymentsystem.paymentservice.domain.enums.Currency;
 
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.util.Currency;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -33,4 +34,14 @@ public class Account {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Currency currency;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.balance = BigDecimal.valueOf(0);
+        this.number = UUID.randomUUID().toString().replace("-", "").substring(0, 12);
+    }
 }
