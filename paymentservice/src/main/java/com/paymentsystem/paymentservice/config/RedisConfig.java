b45a9628328d4448342520e43660f953cc898998
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-05 19:33:27 +0530

    update(paymentservice): Updated KV with String:String

--- paymentservice/src/main/java/com/paymentsystem/paymentservice/config/RedisConfig.java
+++ paymentservice/src/main/java/com/paymentsystem/paymentservice/config/RedisConfig.java
@@ -7,12 +7,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.UUID;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, TransactionStatus> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, TransactionStatus> template = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
