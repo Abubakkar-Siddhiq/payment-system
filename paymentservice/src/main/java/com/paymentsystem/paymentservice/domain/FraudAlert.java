eaec1e86470e5ec027f0c9615a7ad2ea7a9ed4c9
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-12 21:37:43 +0530

    feat(paymentService): Added FraudAlert class

--- paymentservice/src/main/java/com/paymentsystem/paymentservice/domain/FraudAlert.java
+++ paymentservice/src/main/java/com/paymentsystem/paymentservice/domain/FraudAlert.java
@@ -1,4 +1,25 @@
 package com.paymentsystem.paymentservice.domain;
 
+import com.paymentsystem.paymentservice.domain.enums.RiskLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FraudAlert {
+    private UUID id;
+    private UUID transactionId;
+    private UUID senderId;
+    private BigDecimal amount;
+    private String reason;
+    private RiskLevel riskLevel;
+    private LocalDateTime detectedAt;
 }
