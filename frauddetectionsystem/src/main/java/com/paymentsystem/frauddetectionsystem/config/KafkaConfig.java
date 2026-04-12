6fcf7256b3b97589c8825754a22b7fd54954fa8e
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-12 21:30:49 +0530

    update(fraudDetectionSystem): Added paymentTopic Bean and @EnableKafka nnotation

--- frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/config/KafkaConfig.java
+++ frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/config/KafkaConfig.java
@@ -3,13 +3,20 @@ package com.paymentsystem.frauddetectionsystem.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Bean
     public NewTopic fraudAlertTopic() {
         return TopicBuilder.name("fraud-alert-topic").partitions(1).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic paymentTopic() {
+        return TopicBuilder.name("payment-topic").partitions(1).replicas(1).build();
     }
 }
