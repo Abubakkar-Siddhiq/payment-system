812bd12043cdc01697fca7711834c2a22a465352
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-12 21:28:35 +0530

    update(fraudDetectionSystem): Updated for loop with Optional and break to prevent overwriting

--- frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/services/FraudRuleEngine.java
+++ frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/services/FraudRuleEngine.java
@@ -6,14 +6,17 @@ import com.paymentsystem.frauddetectionsystem.kafka.event.TransactionEvent;
 import com.paymentsystem.frauddetectionsystem.repositories.FraudAlertRepository;
 import com.paymentsystem.frauddetectionsystem.rules.FraudRule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FraudRuleEngine {
 
     private final List<FraudRule> rules;
@@ -26,14 +29,18 @@ public class FraudRuleEngine {
         List<FraudAlert> alerts = new ArrayList<>();
 
         for (FraudRule rule : rules) {
-            rule.apply(event).ifPresent(alert -> {
-                FraudAlert saved = repository.save(alert);
-
-                String alertMessage = objectMapper.writeValueAsString(alert);
-                eventProducer.publishFraudAlertEvent(alertMessage);
-
+            Optional<FraudAlert> alert = rule.apply(event);
+            if(alert.isPresent()) {
+                FraudAlert saved = repository.save(alert.get());
+                try {
+                    String alertMessage = objectMapper.writeValueAsString(saved);
+                    eventProducer.publishFraudAlertEvent(alertMessage);
+                } catch (Exception e) {
+                    log.error("Failed to serialize fraud alert: {}", e.getMessage());
+                }
                 alerts.add(saved);
-            });
+                break;
+            }
         }
 
         return alerts;
