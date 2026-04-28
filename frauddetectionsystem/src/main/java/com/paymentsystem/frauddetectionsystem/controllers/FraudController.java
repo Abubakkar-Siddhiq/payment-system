c1bc20bbc86586e591427195ad23504318542229
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-04-28 21:10:19 +0530

    update(fraudDetectionService): Added Approve and Reject alerts in FraudController and FraudService

--- frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/controllers/FraudController.java
+++ frauddetectionsystem/src/main/java/com/paymentsystem/frauddetectionsystem/controllers/FraudController.java
@@ -2,14 +2,13 @@ package com.paymentsystem.frauddetectionsystem.controllers;
 
 
 import com.paymentsystem.frauddetectionsystem.domain.dto.FraudAlertResponse;
+import com.paymentsystem.frauddetectionsystem.domain.entity.FraudAlert;
 import com.paymentsystem.frauddetectionsystem.mappers.FraudAlertMapper;
 import com.paymentsystem.frauddetectionsystem.repositories.FraudAlertRepository;
+import com.paymentsystem.frauddetectionsystem.services.FraudService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +20,7 @@ public class FraudController {
 
     private final FraudAlertRepository fraudAlertRepository;
     private final FraudAlertMapper fraudAlertMapper;
+    private final FraudService fraudService;
 
     @GetMapping
     public ResponseEntity<List<FraudAlertResponse>> getAlerts() {
@@ -43,4 +43,21 @@ public class FraudController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/{id}/approve")
+    public ResponseEntity<FraudAlertResponse> approve(
+            @PathVariable UUID id,
+            @RequestHeader("X-User-Id") String reviewedBy
+    ) {
+        FraudAlert alert = fraudService.approve(id, reviewedBy);
+        return ResponseEntity.ok(fraudAlertMapper.toFraudAlertResponse(alert));
+    }
+
+    @PutMapping("/{id}/reject")
+    public ResponseEntity<FraudAlertResponse> reject(
+            @PathVariable UUID id,
+            @RequestHeader("X-User-Id") String reviewedBy
+    ) {
+        FraudAlert alert = fraudService.reject(id, reviewedBy);
+        return ResponseEntity.ok(fraudAlertMapper.toFraudAlertResponse(alert));
+    }
 }
